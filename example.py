26bd81a018b0137ad9db3472b92c4ae94e7cdb81
Jillazquez <117284941+Jillazquez@users.noreply.github.com>
2024-11-27 19:06:00 +0100

    add feature

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 OK_RESULT = 'yes'
 FATAL_RESULT = 'no'
+import pdb
 
 def check_if_aprove_or_not()-> str:
     result = input("Eres del barça?")
@@ -7,5 +8,12 @@ def check_if_aprove_or_not()-> str:
     if isinstance(result, str):
         if result.lower() == OK_RESULT:
             return 'Sabes de futbol'
+        for x in range(0,15):
+            r = input("Seguro?")
+            pdb.set_trace()
+            if isinstance(r,str):
+                return "Salvado por la campana"
         return 'No sabes de futbol'
     return 'suspendido por ir de listo'
+
+check_if_aprove_or_not()
